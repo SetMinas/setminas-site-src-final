70baeca3899ba085ffe9e27c42802ff6f527cb1b
TiagoCarnevalli <tiagocelias2005@hotmail.com>
2025-11-09 11:09:48 -0300

    Video source fix

--- src/components/sections/home/player.tsx
+++ src/components/sections/home/player.tsx
@@ -1,15 +1,15 @@
 "use client";
 
 import { useState } from "react";
-import { ListBlobResultBlob } from "@vercel/blob";
+// import { ListBlobResultBlob } from "@vercel/blob";
 
-export default function Player({ videos }: { videos: ListBlobResultBlob[] }) {
+export default function Player() {
 	const [isPlaying, setIsPlaying] = useState(true);
 
 	return (
 		<section
 			className={`bg-gray-100 z-40 flex justify-center items-center transition-[max-height] duration-500 ease-in-out ${
-				videos && videos.length > 0 && isPlaying ? "max-h-[90vh]" : "max-h-0"
+				isPlaying ? "max-h-[90vh]" : "max-h-0"
 			}`}
 		>
 			<video
@@ -21,10 +21,6 @@ export default function Player({ videos }: { videos: ListBlobResultBlob[] }) {
 				onEnded={() => setIsPlaying(false)}
 				className="w-full h-auto max-h-[90vh] object-cover"
 			>
-				{videos &&
-					videos.map((video) => (
-						<source key={video.url} src={video.url} type="video/mp4" />
-					))}
 				<source
 					key="default-video"
 					src="/videos/setminas-video-low.mp4"
