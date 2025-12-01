0eb4c62daa420eff569d58af6c810105b391bb47
TiagoCarnevalli <tiagocelias2005@hotmail.com>
2025-12-01 20:53:07 -0300

    Feature:New Google tag added

--- src/lib/googleAds.ts
+++ src/lib/googleAds.ts
@@ -7,6 +7,7 @@ declare global {
 const GOOGLE_ADS_ID = process.env.NEXT_PUBLIC_GOOGLE_ADS_ID!;
 const CONTACT_LABEL = process.env.NEXT_PUBLIC_GOOGLE_ADS_CONTACT_LABEL!;
 const VIEW_LABEL = process.env.NEXT_PUBLIC_GOOGLE_ADS_VIEW_LABEL!;
+const ALLOTMENT_VIEW_LABEL = process.env.NEXT_PUBLIC_GOOGLE_ADS_ALLOTMENT_VIEW_LABEL!;
 
 export function adsConversionContact(source: Record<string, string>) {
   if (typeof window !== "undefined" && window.gtag) {
@@ -21,6 +22,15 @@ export function adsConversionView(page: Record<string, string>) {
   if (typeof window !== "undefined" && window.gtag) {
     window.gtag("event", "conversion", {
       send_to: `${GOOGLE_ADS_ID}/${VIEW_LABEL}`,
+      ...page,
+    });
+  }
+}
+
+export function adsConversionAllotmentView(page: Record<string, string>) {
+  if (typeof window !== "undefined" && window.gtag) {
+    window.gtag("event", "conversion", {
+      send_to: `${GOOGLE_ADS_ID}/${ALLOTMENT_VIEW_LABEL}`,
       ...page,
     });
   }
